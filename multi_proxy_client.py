0ee3c23693dd87fb58cb426abaf377ffc24d19f4
solitariaa <66712118+solitariaa@users.noreply.github.com>
2022-01-18 00:22:04 -0700

    first commit

---
+++ multi_proxy_client.py
@@ -0,0 +1,33 @@
+import socket
+from multiprocessing import Pool
+
+#define address, buffer size
+HOST = "localhost"
+PORT = 8001
+BUFFER_SIZE = 1024
+
+payload = f'GET / HTTP/1.0\r\nHost: www.google.com\r\n\r\n'
+
+def connect(addr):
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect(addr)
+        s.sendall(payload.encode())
+        s.shutdown(socket.SHUT_WR)
+
+        full_data = s.recv(BUFFER_SIZE)
+        print(full_data)
+    
+    except Exception as e:
+        print(e)
+    finally:
+        s.close()
+
+def main():
+    address = [(HOST,PORT)]
+    #establish 10 different connections
+    with Pool() as p:
+        p.map(connect,address*10)
+
+if __name__ == "__main__":
+    main()
